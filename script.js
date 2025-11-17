2e739728344233f262065085b1e5727d79d21967
AmaniTlili <ameni1tlili@gmail.com>
2025-11-17 16:20:37 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        section.style.opacity = 0;
+    });
+
+    window.addEventListener('scroll', () => {
+        const scrollPosition = window.scrollY + window.innerHeight;
+        sections.forEach(section => {
+            if (scrollPosition > section.offsetTop + 100) {
+                section.style.transition = 'opacity 1s';
+                section.style.opacity = 1;
+            }
+        });
+    });
+});
